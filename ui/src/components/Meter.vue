2be821e1f206352529325cf0689f4d74683d097e
mixmix <whimful@gmail.com>
2021-07-22 11:49:09 +1200

    tidy up dashboard + meter design

--- ui/src/components/Meter.vue
+++ ui/src/components/Meter.vue
@@ -1,10 +1,9 @@
 <template>
   <v-card class="mt-4" max-width="400">
     <v-sheet class="v-sheet--offset mx-auto" color="primary" tile>
-      <v-card-text class="pt-0">
-        <v-row class="mt-2 ml-4" align="center">
-          <h2>{{title}}</h2>
-          <span class="pl-4 caption">usage on last hour</span>
+      <v-card-text>
+        <v-row class="mt-0 ml-0" align="end">
+          <h2>{{title}}</h2> <span class="pl-4 caption">usage in last hour</span>
         </v-row>
       </v-card-text>
       <v-sparkline :labels="labels" :value="values" color="cyan" line-width="1" padding="16"></v-sparkline>
@@ -24,9 +23,9 @@ export default {
 </script>
 <style scoped lang="scss">
 h2 {
-    font-size: 1.3em;
-    text-transform: uppercase;
-    font-weight: 400;
-    letter-spacing: 4px;
+  font-size: 1.3em;
+  text-transform: uppercase;
+  font-weight: 400;
+  letter-spacing: 4px;
 }
 </style>
